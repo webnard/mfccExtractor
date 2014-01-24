d4cc97c97eb67ca8b9bda81b9c378ba8a43a272c
David Ireland <Maxwell79@users.noreply.github.com>
2014-01-24 10:33:05 +1000

    Create mfcc.h
    Initial header file commit.

---
+++ mfcc.h
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+class MFCC {
+	private: 
+		double* data; // Frequency-Domain Data
+		int noFilterBanks; // Number of Filter Banks
+		int NFFT; // Length of the FFT
+		double minFreq; // Minimun Frequency (Hz)
+		double maxFreq; // Maximum Frequency (Hz)
+		double sampleFreq; // Sample Frequency 
+
+		vector<double> centreFreqs;
+		vector<vector<double> >  filterBanks;  // Vector array of the filterbanks 
+		vector<int> bins;  
+		map<int,double> binToFreq; // Frequency Bin -> Frequency (Hz) 
+
+		// Methods		
+		void applyDCT(); // Apply the Discrete Cosine Transformation
+		void applyFilterBank(); // Apply the Filter Banks
+		
+
+	public:
+		// Methods		
+
+		// Constructors 
+		MFCC(int,int,double,double,double);
+		MFCC(double*, int, int, double, double, double);
+
+		// Interaction with MFCC object
+		void initFilterBanks(); // Filter banks can be reinitilised
+		void setSpectrumData(double*); // MFCC pointer to data can be reinitilised 
+		vector<double> getLogCoefficents(); // Main function to obtain the MFCC's 
+
+		void setMinimumFrequency(double);
+		void setMaximumFrequency(double);
+		void setSampleFrequency(double);
+		void setnoFilterBanks(int);
+		void setNFFT(int);
+};
